19e1ec186913eecb183976bc9c910f1c9e15f8d7
Denys TOMCHYSHEN <dtomchys@e2r9p9.unit.ua>
2018-06-24 15:05:36 +0300

    width

---
+++ Bullet.cpp
@@ -0,0 +1,42 @@
+#include "Bullet.hpp"
+
+char const Bullet :: _map[1][1] = {{'-'}};
+std :: string Bullet :: bul_t = "bullet";
+
+Bullet::Bullet(): Object() {
+}
+
+Bullet::Bullet(int x, int y): Object(x, y) {
+    map = new char *[_heigth];
+    for (int i = 0; i < _heigth; i++)
+    {
+        map[i] = new char[_width];
+        for (int j = 0; j < _width; j++)
+        {
+            map[i][j] = _map[i][j];
+        }
+    }
+    height = _heigth;
+    width = _width;
+    type = bul_t;
+    hp = bul_hp;
+}
+
+Bullet::Bullet(Bullet const &src) {
+    *this = src;
+}
+
+Bullet &Bullet::operator=(Bullet const &src) {
+    this->map = src.map;
+    this->height = src.height;
+    this->width = src.height;
+    this->x = src.x;
+    this->y = src.y;
+    return (*this);
+}
+
+Bullet::~Bullet() {
+    for (int i = 0; i < height; i++)
+        delete [] map[i];
+    delete [] map;
+}
--- Bullet.hpp
+++ Bullet.hpp
@@ -5,17 +5,17 @@
 
 class Bullet: public Object {
 private:
-    static char const _map[1][1] = {{'-'}};
+    static char const _map[1][1];
     static const int _heigth = 1;
-    static const int _weight = 1;
+    static const int _width = 1;
     static const int bul_hp = 1;
-    static std :: string bul_t = "bullet";
+    static std :: string bul_t;
 
     Bullet();
 public:
     Bullet(int x, int y);
     Bullet(Bullet const &src);
-    Bullet &operator=(Bullet &src);
+    Bullet &operator=(Bullet const &src);
 
     ~Bullet();
 };
